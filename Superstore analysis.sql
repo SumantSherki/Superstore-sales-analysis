2a934ad211ad2177bb093e0afbba658d605e0d31
SumantSherki <sumantsherki21@gmail.com>
2026-05-18 09:31:11 +0530

    SQL queries for business analysis

--- Superstore analysis.sql
+++ Superstore analysis.sql
@@ -33,7 +33,23 @@ having total_profit<0
 order by total_profit;
 
 #Monthly sales trend
-select monthname(order_date) as Month_name,
+select month(order_date) as Month_name,
 sum(sales) as montly_sales from superstore_analysis
-group by Month(order_date), monthname(order_date)
+group by Month(order_date)
 order by Month(order_date);
+
+#Find the top 5 customers by total spending
+select customer_name, sum(sales) 
+from superstore_analysis
+group by customer_name
+order by sum(sales)
+limit 5;
+
+#Profitable products and loss making products
+select product_name, sum(sales),
+case when sum(sales) > 5000 then 'High profit'
+when sum(sales)>0 then 'Moderate profit'
+else 'Loss making' end as Profit_or_loss
+from superstore_analysis
+group by product_name
+order by sum(sales);
